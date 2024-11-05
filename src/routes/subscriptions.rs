849079deb46aa7218a11310131e95e19776c987c
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 00:23:53 +0900

    Stub for `subscriptions`/`subscribe()` take 2
    All tests are green, but still doesn't do much.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,15 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
 
-pub async fn subscribe() -> HttpResponse {
+// TODO temporarily silencing Clippy's warning about FormData's unused fields
+// TODO rm once we have a proper implementation of `subscribe`.
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    #[allow(unused)]
+    email: String,
+    #[allow(unused)]
+    name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
