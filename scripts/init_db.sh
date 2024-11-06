db26512c512a00db4e46e12285dd247d90846848
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 23:21:35 +0900

    Add DB migration machinery
    Add `migrations` directory, and sqlx-cli code that leverages it for
    migrations to `init_db.sh`, per section 3.8.4.
    
    Also make init_db.sh more robust and flexible by adding checks for
    `sqlx-cli`'s presence, and the option to skip spinning up a new postgres
    container.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,14 @@
 set -x
 set -euo pipefail
 
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 # Check if a custom parameter has been set, otherwise use default values
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
@@ -21,35 +29,53 @@ APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 HOST_ADDR="127.0.0.1"
 HOST_PORT="5432"
 
-# Launch postgres using Docker
-CONTAINER_NAME="postgres"
-docker run \
-  --env POSTGRES_USER="${SUPERUSER}" \
-  --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
-  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-  --health-interval=1s \
-  --health-timeout=5s \
-  --health-retries=5 \
-  --publish  ${HOST_ADDR}:"${HOST_PORT}":"${DB_PORT}" \
-  --detach \
-  --name "${CONTAINER_NAME}" \
-postgres -N 1000
-# ^ Increased maximum number of connections for testing purposes
+# Allow to skip Docker if a dockerized Postgres database is already running
+SKIP_DOCKER="${SKIP_DOCKER:=""}"
+ if [[ -z "${SKIP_DOCKER}" ]]
+then
+  # Launch postgres using Docker
+  CONTAINER_NAME="postgres"
+  docker run \
+    --env POSTGRES_USER="${SUPERUSER}" \
+    --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish  ${HOST_ADDR}:"${HOST_PORT}":"${DB_PORT}" \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+  postgres -N 1000
+  # ^ Increased maximum number of connections for testing purposes
 
-# Wait for Postgres to be ready to accept connections
-until [ \
-  "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-  "healthy" \
-]; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
-done
->&2 echo "Postgres is now available on port ${DB_PORT}!"
+  # Wait for Postgres to be ready to accept connections
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+  done
 
-# Create the application user
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+  # Create the application user
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
 
-# Grant CREATEDB privileges to the application user
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+  # Grant CREATEDB privileges to the application user
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+ fi
+
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+# Create the application database
+if [ -d migrations ]; then
+  DATABASE_URL="postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}"
+  export DATABASE_URL
+  sqlx database create
+  sqlx migrate run
+  >&2 echo "Postgres has been migrated, ready to go!"
+else
+  >&2 echo "The 'migrations' directory does not exist! Aborting."
+fi
+
