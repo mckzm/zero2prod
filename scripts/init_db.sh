a58420bc6cfbe30f249661d63364dd7bf0965a3b
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 19:50:08 +0900

    Add comments to document init_db.sh
    Specifically, the way we handle port mapping, and remaining caveats
    owing to Moby issue #45610.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -9,6 +9,14 @@ SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
 
 # Launch postgres using Docker
 CONTAINER_NAME="postgres"
+
+# IP address/port publishing parameters
+# If an IP address is not specified, Docker publishes the port to 0.0.0.0, IOW
+# all interfaces, per <https://docs.docker.com/engine/network/#published-ports>
+# and <https://docs.docker.com/reference/cli/docker/container/run/#publish>.
+# Per the latter, note that Docker manages its own iptables rules.
+# Note as well that hosts within the same L2 segment can reach ports published
+# to localhost, per issue #45610: <https://github.com/moby/moby/issues/45610>
 LISTEN_ADDR="127.0.0.1"
 HOST_PORT="5432"
 CONTAINER_PORT="5432"
