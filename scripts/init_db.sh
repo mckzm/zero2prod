b5d6e14c0f9e6dafdb78b28d23e5e6a8f6b50133
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 21:31:19 +0900

    Create app user and grant it CREATEDB privileges

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,9 +6,10 @@ set -euo pipefail
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
 SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
-# Launch postgres using Docker
-CONTAINER_NAME="postgres"
 
 # IP address/port publishing parameters
 # If an IP address is not specified, Docker publishes the port to 0.0.0.0, IOW
@@ -20,12 +21,35 @@ CONTAINER_NAME="postgres"
 HOST_ADDR="127.0.0.1"
 HOST_PORT="5432"
 
+# Launch postgres using Docker
+CONTAINER_NAME="postgres"
 docker run \
   --env POSTGRES_USER="${SUPERUSER}" \
   --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
+  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+  --health-interval=1s \
+  --health-timeout=5s \
+  --health-retries=5 \
   --publish  ${HOST_ADDR}:"${HOST_PORT}":"${DB_PORT}" \
   --detach \
   --name "${CONTAINER_NAME}" \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
-#--publish ${IP_ADDR}:"${DB_PORT}":5432 \
+
+# Wait for Postgres to be ready to accept connections
+until [ \
+  "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+  "healthy" \
+]; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "Postgres is now available on port ${DB_PORT}!"
+
+# Create the application user
+CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+
+# Grant CREATEDB privileges to the application user
+GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
