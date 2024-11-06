2d4f281a1d905c8fcccab82ccafec7ad1af1712b
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 17:01:11 +0900

    Add TODO to `subscribe()`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,9 @@ pub struct FormData {
     name: String,
 }
 
+// TODO We'll probably want to send back a 201 here when successful once we
+// have more than a stub, per:
+// <https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201>
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
