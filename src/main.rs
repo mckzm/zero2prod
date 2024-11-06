4e1e62f83e8078adc3f453934d4481c3a63c7c70
mckzm <morphisms.everywhere@gmail.com>
2024-11-06 20:06:16 +0900

    Parameterise server's IP address and port

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to socket");
+    let socket_spec = "127.0.0.1:8080";
+    let listener = std::net::TcpListener::bind(socket_spec).expect("Failed to bind to socket");
     run(listener)?.await
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,9 +2,10 @@ use std::net::TcpListener;
 use zero2prod::startup::run;
 
 pub fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to socket");
+    let socket_spec = "127.0.0.1:0";
+    let listener = TcpListener::bind(socket_spec).expect("Failed to bind to socket");
     let port = listener.local_addr().unwrap().port();
     let server = run(listener).expect("Failed to bind to given socket");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    format!("http://{}:{}", socket_spec.trim_end_matches(":0"), port)
 }
